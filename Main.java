ff8f3ed8fc590defb950e4ead3e9ab5d674fe45c
rebeccapeltz <rebecca.peltz@cloudinary.com>
2021-03-26 10:41:44 -0700

    first

---
+++ Main.java
@@ -0,0 +1,12 @@
+import com.cloudinary.Cloudinary;
+import com.cloudinary.Api;
+import com.cloudinary.utils.ObjectUtils;
+
+
+class Main {
+  public static void main(String[] args) throws Exception {
+    AestheticsTransformations at=new AestheticsTransformations();
+    at.printTransformation();
+       
+  }
+}
